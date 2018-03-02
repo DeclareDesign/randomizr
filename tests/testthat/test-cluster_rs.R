f359d81339e563918e839eeec2202cb11efad291
Neal Fultz <nfultz@gmail.com>
2018-03-02 22:03:19 +0000

    cluster_rs

--- tests/testthat/test-cluster_rs.R
+++ tests/testthat/test-cluster_rs.R
@@ -1,30 +1,41 @@
 
 context("Cluster Random Sampling")
+clusters <- rep(letters[1:15], times = 1:15)
 
 #debugonce(cluster_rs_probabilities)
-# Two Group Designs
-clusters <- rep(letters, times = 1:26)
-S <- cluster_rs(clusters = clusters)
-table(S, clusters)
-probs <- cluster_rs_probabilities(clusters = clusters)
-table(probs, clusters)
+test_that("Two Group Designs", {
+  S <- cluster_rs(clusters = clusters)
+  probs <- cluster_rs_probabilities(clusters = clusters)
+  expect_true(all(colSums(table(S, clusters) != 0) == 1))
+  expect_true(all(probs == .5))
+})
 
-S <- cluster_rs(clusters = clusters, n = 13)
-table(S, clusters)
-probs <- cluster_rs_probabilities(clusters = clusters, n = 13)
-table(probs, clusters)
+test_that("Two Gropu n = 13", {
+  S <- cluster_rs(clusters = clusters, n = 13)
+  probs <- cluster_rs_probabilities(clusters = clusters, n = 13)
+  expect_true(all(colSums(table(S, clusters) != 0) == 1))
+  expect_true(all(probs == 13/15))
+})
 
-S <- cluster_rs(clusters = clusters, prob = .5)
-table(S, clusters)
-probs <- cluster_rs_probabilities(clusters = clusters, prob = .5)
-table(probs, clusters)
+test_that("prob = .5", {
+  S <- cluster_rs(clusters = clusters, prob = .5)
+  probs <- cluster_rs_probabilities(clusters = clusters, prob = .5)
+  expect_true(all(colSums(table(S, clusters) != 0) == 1))
+  expect_true(all(probs == .5))
+})
 
+test_that("prob = 0", {
+  
 S <- cluster_rs(clusters = clusters, prob = 0)
-table(S, clusters)
 probs <- cluster_rs_probabilities(clusters = clusters, prob = 0)
-table(probs, clusters)
+expect_true(all(S== 0))
+expect_true(all(probs== 0))
+})
 
-S <- cluster_rs(clusters = clusters, prob = 1)
-table(S, clusters)
-probs <- cluster_rs_probabilities(clusters = clusters, prob = 1)
-table(probs, clusters)
+test_that("prob = 1", {
+  
+  S <- cluster_rs(clusters = clusters, prob = 1)
+  probs <- cluster_rs_probabilities(clusters = clusters, prob = 1)
+  expect_true(all(S== 1))
+  expect_true(all(probs== 1))
+})
