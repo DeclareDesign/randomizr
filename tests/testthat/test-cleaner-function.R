635094bc1cad1d71ff64943636e4285e3d1cd3d7
Neal Fultz <nfultz@gmail.com>
2018-03-02 21:51:59 +0000

    cleaner

--- tests/testthat/test-cleaner-function.R
+++ tests/testthat/test-cleaner-function.R
@@ -1,20 +1,21 @@
 context("Cleaner Function")
 
 
-# throws a dumb warning in test that
-#expect_error(check_randomizr_arguments(N = "a"))
-expect_error(check_randomizr_arguments(N = -1))
-
-expect_error(check_randomizr_arguments(N = 100, prob = -1))
-expect_error(check_randomizr_arguments(N = 100, prob = 2))
-
-expect_error(check_randomizr_arguments(N = 100, m = -1))
-expect_error(check_randomizr_arguments(N = 100, m = 101))
-
-expect_error(check_randomizr_arguments(N = 100, m_each = c(-1, 2, 99)))
-expect_error(check_randomizr_arguments(N = 100, m_each = c(1, 2, 99)))
-
-expect_error(check_randomizr_arguments(N = 100, prob_each = c(-.5, 1.5)))
-expect_error(check_randomizr_arguments(N = 100, prob_each = c(.1,.2)))
-
+test_that("checking randomizr argumnets", {
+  expect_error(expect_warning(check_randomizr_arguments(N = "a")))
+    
+  expect_error(check_randomizr_arguments(N = -1))
+  
+  expect_error(check_randomizr_arguments(N = 100, prob = -1))
+  expect_error(check_randomizr_arguments(N = 100, prob = 2))
+  
+  expect_error(check_randomizr_arguments(N = 100, m = -1))
+  expect_error(check_randomizr_arguments(N = 100, m = 101))
+  
+  expect_error(check_randomizr_arguments(N = 100, m_each = c(-1, 2, 99)))
+  expect_error(check_randomizr_arguments(N = 100, m_each = c(1, 2, 99)))
+  
+  expect_error(check_randomizr_arguments(N = 100, prob_each = c(-.5, 1.5)))
+  expect_error(check_randomizr_arguments(N = 100, prob_each = c(.1,.2)))
+})
 
