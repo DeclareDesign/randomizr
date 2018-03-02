da7cd2edb14648af9f2399a8fa00d857f411e900
Neal Fultz <nfultz@gmail.com>
2018-03-02 20:09:45 +0000

    test-condition-names.R

--- tests/testthat/test-condition-names.R
+++ tests/testthat/test-condition-names.R
@@ -1,28 +1,60 @@
 context("Condition Names")
 
 
+expect_u <- function(draw, l){
+  expect_true(all(draw %in% l))
+}
+
 test_that("Condition Names", {
   
   N <- 10
-  complete_ra(10, 5)
-  complete_ra(N = N, num_arms = 2)
-  complete_ra(N = N, num_arms = 3)
-  complete_ra(N = N, m = 3, conditions = c(T, F))
-  complete_ra(N = N, m = 3, conditions = c(F, T))
+  
+  expect_u(complete_ra(N, 5), 0:1)
+  
+
+  expect_u(
+    complete_ra(N = N, num_arms = 2), 
+    c("T1", "T2")
+  )
+  
+  expect_u(
+    complete_ra(N = N, num_arms = 3), 
+    c("T1", "T2", "T3")
+  )
+  
+  expect_u(
+    complete_ra(N = N, m = 3, conditions = c(T, F)),
+    c(TRUE, FALSE)
+  )
+  
+  expect_u(
+    complete_ra(N = N, m = 3, conditions = c(F, T)),
+    c(FALSE, TRUE)
+  )
+  
+  expect_u(
+    complete_ra(10, conditions = c("C", "B", "A")),
+    c("C", "B", "A")
+  )
+
   expect_error(complete_ra(100, conditions = c("control", "control", "treatment")))
-  
-  complete_ra(10, conditions = c("C", "B", "A"))
-  
+})
 
 # factors -----------------------------------------------------------------
 
-
+test_that("Condition Names w/ factors", {
+    
   fact_1 <- factor(c("A", "B"), levels = c("A", "B"))
   fact_2 <- factor(c("A", "B"), levels = c("B", "A"))
     
-  complete_ra(N, conditions = fact_1)
-  complete_ra(N, conditions = fact_2)
+  expect_u(
+    complete_ra(N, conditions = fact_1), 
+    fact_1
+  )
   
-  
-  
+  expect_u(
+    complete_ra(N, conditions = fact_2), 
+    fact_2
+  )
+
 })
